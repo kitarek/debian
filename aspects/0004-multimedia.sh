fb0a9dda792dc9b98a09badfe911ba0b192f4e2b
kitarek <arkadiusz.kita@gmail.com>
2013-12-11 21:38:06 +0100

    Fixed GPG key error (NO_PUBKEY).
      * Fixed keys errors for additionals repositories
      * Fetching public keys and adding them to apt package manager

--- aspects/0004-multimedia.sh
+++ aspects/0004-multimedia.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+apt-get install -qq debian-keyring
 echo "deb http://debian.oppserver.net/xbmc/ wheezy main non-free contrib" > /etc/apt/sources.list.d/oppxbmc.sources.list
 echo "deb-src http://debian.oppserver.net/xbmc/ wheezy main non-free contrib" >> /etc/apt/sources.list.d/oppxbmc.sources.list
 echo "deb-src http://debian.oppserver.net/xbmc/ wheezy main non-free contrib" > /etc/apt/sources.list.d/deb-multimedia.sources.list
@@ -6,4 +7,6 @@ echo "deb http://www.deb-multimedia.org/ wheezy main non-free" >> /etc/apt/sourc
 echo "deb-src http://www.deb-multimedia.org/ wheezy main" >> /etc/apt/sources.list.d/deb-multimedia.sources.list
 echo "deb http://www.deb-multimedia.org/ wheezy-backports main" >> /etc/apt/sources.list.d/deb-multimedia.sources.list
 echo "deb-src http://www.deb-multimedia.org/ wheezy-backports main" >> /etc/apt/sources.list.d/deb-multimedia.sources.list
+wget -q http://debian.oppserver.net/gpg-debian.oppserver.net-signing-pubkey.asc -O - | apt-key add -
+gpg --keyring /usr/share/keyrings/debian-keyring.gpg -a --export 07DC563D1F41B907 | apt-key add -
 apt-get -qq update
