85c60ee424f293fd2a7df82fb1f239f6a9ebd5d0
kitarek <arkadiusz.kita@gmail.com>
2013-12-11 23:20:34 +0100

    Fixed dependency firefox problem
      * Fixed inconsistent packages tree state after installing pencil
      * Creating dummy firefox package on the fly
      * Installing pencil without skipping deps resolution

--- aspects/0022-pencil.sh
+++ aspects/0022-pencil.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
+apt-get install -qq -y equivs
+equivs-control /tmp/firefox-template
+cat /tmp/firefox-template | egrep '(Standards-Version:|Priority:|Section:)' > /tmp/firefox 
+echo """
+Package: firefox
+Version: 3.0.0
+Maintainer: Arkadiusz Kita <arkadiusz.kita@gmail.com>
+Architecture: all
+Description: A dummy firefox package to fool Debian's package management system (for purposes of Pencil package)
+""" >> /tmp/firefox
+equivs-build /tmp/firefox > /dev/null
+mv firefox_3.0.0_all.deb /tmp/firefox_3.0.0_all.deb
+dpkg -i /tmp/firefox_3.0.0_all.deb >/dev/null
+
 file="/tmp/evoluspencil_2.0.5_all.deb"
 wget -q -O "${file}" 'http://evoluspencil.googlecode.com/files/evoluspencil_2.0.5_all.deb'
-dpkg --force-depends -i "${file}" >/dev/null
+dpkg -i "${file}" >/dev/null
 rm "${file}"
 
