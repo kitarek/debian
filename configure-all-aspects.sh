67e23a3deadef6ed6e8218830c4c885b71e3843f
kitarek <arkadiusz.kita@gmail.com>
2013-12-11 21:12:03 +0100

    Added required parameter user login.
    * Added usage message for main script.

--- configure-all-aspects.sh
+++ configure-all-aspects.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+if [ ${#} -ne 1 ]; then
+    echo "Configure system using root account but for specified user only (provided as parameter)"
+    echo "Usage: ${0} unix-login"
+    exit 1
+fi
 
 cmd=`readlink -f ${0}`
 cmddir=`dirname $cmd`
 
 for file in `ls -1 ${cmddir}/aspects/*.sh`; do
 	echo "$file: running"
-	/bin/sh $file
+	/bin/sh $file ${1}
 	echo "$file: exited with ${?}"
 done
